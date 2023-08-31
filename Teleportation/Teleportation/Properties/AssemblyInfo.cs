6622948d1ba0c4d1d3f36ad27df240ec87e51fd0
FrederickDietz <141902673+FrederickDietz@users.noreply.github.com>
2023-08-31 14:59:13 -0400

    AssemblyInfo.cs
    added a translation.

--- Teleportation/Teleportation/Properties/AssemblyInfo.cs
+++ Teleportation/Teleportation/Properties/AssemblyInfo.cs
@@ -5,6 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
+
+//Translation:
+//General information about an assembly is provided by the following 
+//attributes. Modify these attribute values to change the information, 
+//that are linked to an assembly.
 [assembly: AssemblyTitle("Teleportation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -17,9 +22,14 @@ using System.Runtime.InteropServices;
 // Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
 // für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
 // COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+
+//Setting ComVisible to false makes the types in this assembly invisible
+//for COM components. If you click on a type in this assembly of
+//COM, set the ComVisible attribute to true for this type.
 [assembly: ComVisible(false)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+// The following GUID determines the ID of the type library when this project is exposed to COM
 [assembly: Guid("fbe543c6-85e2-4633-8d86-883c42a67b36")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
@@ -31,6 +41,14 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
+
+//Version information for an assembly consists of the following four values:
+// Major version
+// Minor version
+// Buildnumber
+// Revision
+//You can specify all the values or the default build and revision numbers
+//by typing "*":
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
